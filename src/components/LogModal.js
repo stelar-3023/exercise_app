d6b7b807545416c408ac5ea0bf3e57138a85d005
Steve Larsen <slarsen-3@att.net>
2021-06-08 04:13:05 -0500

    added delete workout

--- src/components/LogModal.js
+++ src/components/LogModal.js
@@ -1,14 +1,15 @@
 import React, { Component } from "react";
-import { Modal, ModalBody, Table } from "reactstrap";
+import { Button, Form, FormGroup, Modal, ModalBody } from "reactstrap";
+import firebase from "../config/fire";
 
 class LogModal extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      exercise: "",
-      reps: [],
-      isLogWorkoutOpen: false,
+      workouts: [],
+      isLogOpen: false,
     };
+    this.exerciseRef = firebase.firestore().collection("workouts").doc("Hw1CF8bbizlDSH7VTcvF");
     // bind methods
     this.toggleLog = this.toggleLog.bind(this);
     this.handleChange = this.handleChange.bind(this);
@@ -24,6 +25,37 @@ class LogModal extends Component {
     this.setState({ [event.target.name]: event.target.value });
   }
 
+ 
+
+  getWorkouts() {
+    this.exerciseRef.onSnapshot((snapshot) => {
+      console.log(snapshot);
+      let workouts = snapshot.data().exercises
+      console.log(workouts);
+      this.setState({ workouts });
+    });
+  }
+
+  // modifyWorkout(modifiedWorkout) {
+  //   this.exerciseRef
+  //   .doc(.id)
+  // }
+
+  deleteWorkout(workout) {
+    this.exerciseRef
+      .set({exercises: this.state.workouts.filter((_workout) => {
+        return _workout.id !== workout.id
+      })})
+      .then(this.getWorkouts)
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  componentDidMount() {
+    this.getWorkouts();
+  }
+
   render() {
     return (
       <React.Fragment>
@@ -36,14 +68,30 @@ class LogModal extends Component {
           toggle={this.toggleLog}
         >
           <ModalBody>
-            <Table>
-              <thead>
-                <tr>
-                  <th>Exercise</th>
-                  <th>Reps</th>
-                </tr>
-              </thead>
-            </Table>
+            <h2>Workout</h2>
+            <Form>
+              <FormGroup>
+                {this.state.workouts.map((workout) => (
+                  <div key={workout.id}>
+                    <p>{workout.exercise}</p>
+                    <p>{workout.reps}</p>
+                    <br />
+                    <Button typeo="submit" color="danger" size="sm">
+                      Modify
+                    </Button>
+                    <span>&nbsp;&nbsp;</span>
+                    <Button
+                      onClick={() => this.deleteWorkout(workout)}
+                      typeo="submit"
+                      color="danger"
+                      size="sm"
+                    >
+                      Delete
+                    </Button>
+                  </div>
+                ))}
+              </FormGroup>
+            </Form>
           </ModalBody>
         </Modal>
       </React.Fragment>
