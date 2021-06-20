3f391607276364e56ed03a8b87684ae85b09417b
Steve Larsen <slarsen-3@att.net>
2021-06-19 20:11:53 -0500

    add __tests__

---
+++ src/components/__tests__/LogModal.test.js
@@ -0,0 +1,7 @@
+const toggleLog = require("../LogModal");
+
+describe("The toggleLog function should open the Log Modal", () => {
+  it("toggleLog function exists", () => {
+    expect(toggleLog).toBeDefined();
+  });
+});
